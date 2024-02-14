901544307148693d9b9d010f1107808673dbe87b
ksvslk <indrek.luts@outlook.com>
2024-02-14 22:32:33 +0200

    Update angular and add pwa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,5 +10,8 @@ export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideServiceWorker('ngsw-worker.js', {
         enabled: !isDevMode(),
         registrationStrategy: 'registerWhenStable:30000'
+    }), provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
     })]
 };
